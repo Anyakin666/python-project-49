0fbb960064bab17531067cecb240388e439f62ee
Anyakin <aninasvi@gmail.com>
2026-02-18 17:32:10 +0300

    step 8

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,10 @@
+import random
+import math
+RULES = 'Find the greatest common divisor of given numbers.'
+
+
+def get_question_and_answer():
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    correct = math.gcd(number1, number2)
+    return f"{number1} {number2}", str(correct)
