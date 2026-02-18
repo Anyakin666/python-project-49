192d8e15db333279d3a115fff1dc16e2be0131e2
Anyakin <aninasvi@gmail.com>
2026-02-18 17:00:06 +0300

    fixed code

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,31 +1,35 @@
+#!/usr/bin/env python
 import random
-import operator
 import prompt
-
 from brain_games.cli import welcome_user
+
 def main():
     name = welcome_user()
-    print("What is the result of the expression?")
-    operations = {
-        '+': operator.add,
-        '-': operator.sub,
-        '*': operator.mul,
-    }
-    for i in range(3):
-        op_symbol = random.choice(list(operations.keys()))
-        op_func = operations[op_symbol]
-        number1 = random.randint(1, 100)
-        number2 = random.randint(1, 100)
-        number = op_func(number1, number2)
-        print(f"Question: {number1} {op_symbol} {number2}")
-        answer = prompt.string("Your answer: ")
-        if int(answer) == number:
-            print("Correct!")
+    print('What is the result of the expression?')
+    
+    for _ in range(3):
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        op = random.choice(['+', '-', '*'])
+        
+        if op == '+':
+            correct = num1 + num2
+        elif op == '-':
+            correct = num1 - num2
         else:
-            print(
-                f"{answer} is wrong answer ;(. "
-                f"Correct answer was {number}."
-                )
+            correct = num1 * num2
+            
+        print(f'Question: {num1} {op} {num2}')
+        answer = prompt.string('Your answer: ')
+        
+        if int(answer) == correct:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct}'.")
             print(f"Let's try again, {name}!")
             return
-    print(f"Congratulations, {name}!")
+            
+    print(f'Congratulations, {name}!')
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -32,3 +32,5 @@ def main():
     print(f"Congratulations, {name}!")
 
 
+if __name__ == '__main__':
+    main()
