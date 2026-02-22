1943715c972659320f9f8411e4b74f0a8809aaf5
Anyakin <aninasvi@gmail.com>
2026-02-22 14:14:38 +0300

    laaast

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,4 +1,5 @@
 import prompt
+
 from brain_games.cli import welcome_user
 
 
@@ -14,7 +15,8 @@ def run(game_module):
         if user_answer == correct_answer:
             print("Correct!")
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,5 +1,5 @@
-import random
 import math
+import random
 
 RULES = 'Find the greatest common divisor of given numbers.'
 
